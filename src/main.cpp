5c9ecd8447a56842a6285f92e2aa20bbc9419b6c
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-03-23 23:24:53 +0530

    feat: refactor for esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include "Arduino.h"
-#include "SoftwareSerial.h"
 #include "core/bot.h"
+#include "BluetoothSerial.h"
 
-Core::Motor left{2, 3}, right{4, 5};
-SoftwareSerial mySerial(10, 11);
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+Core::Motor left{13, 14}, right{18, 19};
 Core::Bot bot{left, right};
+static char buffer;
 
 
 void setup() {
     Serial.println("Setting up baud rate");
-    Serial.begin(9600);
-    mySerial.begin(9600);
+    Serial.begin(115200);
+    SerialBT.begin("ESPROVER");
     Serial.println("Declaring pins");
     left.declarePins();
     right.declarePins();
@@ -18,52 +23,57 @@ void setup() {
 }
 
 
+char getFirstChar() {
+    char inChar = (char) tolower(SerialBT.read());
+    while (SerialBT.available() > 0) {
+        SerialBT.read();
+    }
+    return (inChar != '\n' && inChar != '\r') ? inChar : buffer;
+}
+
+
 void loop() {
-    if (mySerial.available()) {
-        char inChar = (char) tolower(mySerial.read());
+    if (SerialBT.available()) {
+        char inChar = getFirstChar();
         Serial.print("I received: ");
         Serial.println(inChar);
         if ((inChar >= '1' && inChar <= '9') || 'q' == inChar) {
-            bot.setSpeed((inChar == 'q') ? 100 : abs(int(inChar) * 10));
-            Serial.print("Speed set to: ");
-            Serial.println(int(bot.speed));
+            if ('q' == inChar) {
+                bot.setSpeed(100);
+            } else {
+                bot.setSpeed((inChar - '0') * 10);
+            }
+            Serial.print("Setting speed to ");
+            Serial.println(bot.speed);
         }
-        switch (inChar) {
-            case 'f':
-                Serial.println("Moving forward");
-                bot.linear(true);
-                break;
-            case 'b':
-                Serial.println("Moving backward");
-                bot.linear(false);
-                break;
-            case 'l':
-                Serial.println("Turning left");
-                bot.turn(false);
-                break;
-            case 'r':
-                Serial.println("Turning right");
-                bot.turn(true);
-                break;
-            case 'g':
-                Serial.println("Moving forward left");
-                bot.diagonal(true, false);
-                break;
-            case 'i':
-                Serial.println("Moving forward right");
-                bot.diagonal(true, true);
-                break;
-            case 'h':
-                Serial.println("Moving backward left");
-                bot.diagonal(false, true);
-                break;
-            case 'j':
-                Serial.println("Moving backward right");
-                bot.diagonal(false, false);
-                break;
-            default:
-                bot.stop();
-                break;
+        if ('f' == inChar) {
+            Serial.println("Moving forward");
+            bot.linear(true);
+        } else if ('b' == inChar) {
+            Serial.println("Moving backward");
+            bot.linear(false);
+        } else if ('l' == inChar) {
+            Serial.println("Turning left");
+            bot.turn(false);
+        } else if ('r' == inChar) {
+            Serial.println("Turning right");
+            bot.turn(true);
+        } else if ('g' == inChar) {
+            Serial.println("Moving forward left");
+            bot.diagonal(true, false);
+        } else if ('i' == inChar) {
+            Serial.println("Moving forward right");
+            bot.diagonal(true, true);
+        } else if ('h' == inChar) {
+            Serial.println("Moving backward left");
+            bot.diagonal(false, true);
+        } else if ('j' == inChar) {
+            Serial.println("Moving backward right");
+            bot.diagonal(false, false);
+        } else {
+            bot.stop();
         }
+        buffer = inChar;
+        delay(10);
     }
 }
