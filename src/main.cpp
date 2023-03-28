0d74597170d66d6251e82018e74c4b9490098d22
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-03-28 19:23:29 +0530

    feat: character branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "Arduino.h"
-#include <string>
 #include "core/bot.h"
 #include "BluetoothSerial.h"
 
@@ -10,6 +9,7 @@
 BluetoothSerial SerialBT;
 Core::Motor left{13, 14}, right{18, 19};
 Core::Bot bot{left, right};
+static char buffer;
 
 
 void setup() {
@@ -23,17 +23,57 @@ void setup() {
 }
 
 
+char getFirstChar() {
+    char inChar = (char) tolower(SerialBT.read());
+    while (SerialBT.available() > 0) {
+        SerialBT.read();
+    }
+    return (inChar != '\n' && inChar != '\r') ? inChar : buffer;
+}
+
+
 void loop() {
     if (SerialBT.available()) {
-        std::string line = SerialBT.readStringUntil('\n').c_str();
-        if (line.empty()) {
-            return;
+        char inChar = getFirstChar();
+        Serial.print("I received: ");
+        Serial.println(inChar);
+        if ((inChar >= '1' && inChar <= '9') || 'q' == inChar) {
+            if ('q' == inChar) {
+                bot.setSpeed(100);
+            } else {
+                bot.setSpeed((inChar - '0') * 10);
+            }
+            Serial.print("Setting speed to ");
+            Serial.println(bot.speed);
         }
-        Core::Control left_ { false, 0 }, right_ { false, 0 };
-        Serial.println(line.c_str());
-        left_.fromPayload(line.substr(0, line.find(',')));
-        right_.fromPayload(line.substr(line.find(',') + 1));
-        bot.controlMotors(left_, right_);
+        if ('f' == inChar) {
+            Serial.println("Moving forward");
+            bot.linear(true);
+        } else if ('b' == inChar) {
+            Serial.println("Moving backward");
+            bot.linear(false);
+        } else if ('l' == inChar) {
+            Serial.println("Turning left");
+            bot.turn(false);
+        } else if ('r' == inChar) {
+            Serial.println("Turning right");
+            bot.turn(true);
+        } else if ('g' == inChar) {
+            Serial.println("Moving forward left");
+            bot.diagonal(true, false);
+        } else if ('i' == inChar) {
+            Serial.println("Moving forward right");
+            bot.diagonal(true, true);
+        } else if ('h' == inChar) {
+            Serial.println("Moving backward left");
+            bot.diagonal(false, true);
+        } else if ('j' == inChar) {
+            Serial.println("Moving backward right");
+            bot.diagonal(false, false);
+        } else {
+            bot.stop();
+        }
+        buffer = inChar;
     }
     delay(100);
 }
